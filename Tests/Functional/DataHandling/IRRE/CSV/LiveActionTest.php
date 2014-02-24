63673489f4e95f42cd181dc93d6643d88ff48b0e
Oliver Hader <oliver@typo3.org>
2014-02-24 15:21:18 +0100

    [TASK] Windows issues with long file names in Git sources
    The recent DataHander functional tests integrate file names that
    are very long since they contain a description of what actually
    is expected in the accordant test assertion file.
    
    However, Windows has a limit of 260 characters per file path.
    The longest file path of the mentioned functional tests has been
    223 characters and is now reduced to 166 characters.
    
    Resolves: #56177
    Releases: 6.2
    Change-Id: I1490a3f0fae7ef40547d81e304cb98077ab6142b
    Reviewed-on: https://review.typo3.org/27815
    Reviewed-by: Marc Bastian Heinrichs
    Reviewed-by: Oliver Hader
    Tested-by: Oliver Hader

--- Tests/Functional/DataHandling/IRRE/CSV/LiveActionTest.php
+++ Tests/Functional/DataHandling/IRRE/CSV/LiveActionTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace TYPO3\CMS\Core\Tests\Functional\DataHandling\InlineRelationalRecordEditing\CommaSeparatedValue;
+namespace TYPO3\CMS\Core\Tests\Functional\DataHandling\IRRE\CSV;
 
 /***************************************************************
  * Copyright notice
