0533833d505513619118aada4ffb4d0cfd381feb
Benjamin Mack <benni@typo3.org>
2015-06-20 22:01:20 +0200

    [!!!][TASK] Remove Cache helper class
    The Cache class that acted as a central point when the caching
    framework was optionally enabled for 4.3 is now moved
    into separate methods.
    
    The duplicate code for creating the database statement is now
    moved from the Install Tool to EXT:core to be used in the Extension
    Manager as well.
    
    Resolves: #56319
    Releases: master
    Change-Id: I871a9a6ea506795335e2e8f5b6a410604feba506
    Reviewed-on: http://review.typo3.org/40500
    Reviewed-by: Nicole Cordes <typo3@cordes.co>
    Tested-by: Nicole Cordes <typo3@cordes.co>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ Classes/Cache/DatabaseSchemaService.php
@@ -0,0 +1,72 @@
+<?php
+namespace TYPO3\CMS\Core\Cache;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * This service provides the sql schema for the caching framework
+ */
+class DatabaseSchemaService {
+
+	/**
+	 * Get schema SQL of required cache framework tables.
+	 *
+	 * This method needs ext_localconf and ext_tables loaded!
+	 *
+	 * @return string Cache framework SQL
+	 */
+	public function getCachingFrameworkRequiredDatabaseSchema() {
+		// Use new to circumvent the singleton pattern of CacheManager
+		$cacheManager = new CacheManager;
+		$cacheManager->setCacheConfigurations($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']);
+		// Cache manager needs cache factory. cache factory injects itself to manager in __construct()
+		new CacheFactory('production', $cacheManager);
+
+		$tableDefinitions = '';
+		foreach ($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'] as $cacheName => $_) {
+			$backend = $cacheManager->getCache($cacheName)->getBackend();
+			if (method_exists($backend, 'getTableDefinitions')) {
+				$tableDefinitions .= LF . $backend->getTableDefinitions();
+			}
+		}
+
+		return $tableDefinitions;
+	}
+
+	/**
+	 * A slot method to inject the required caching framework database tables to the
+	 * tables definitions string
+	 *
+	 * @param array $sqlString
+	 * @param string $extensionKey
+	 * @return array
+	 */
+	public function addCachingFrameworkRequiredDatabaseSchemaForInstallUtility(array $sqlString, $extensionKey) {
+		$sqlString[] = $this->getCachingFrameworkRequiredDatabaseSchema();
+		return array($sqlString, $extensionKey);
+	}
+
+	/**
+	 * A slot method to inject the required caching framework database tables to the
+	 * tables definitions string
+	 *
+	 * @param array $sqlString
+	 * @return array
+	 */
+	public function addCachingFrameworkRequiredDatabaseSchemaForSqlExpectedSchemaService(array $sqlString) {
+		$sqlString[] = $this->getCachingFrameworkRequiredDatabaseSchema();
+		return array($sqlString);
+	}
+
+}
