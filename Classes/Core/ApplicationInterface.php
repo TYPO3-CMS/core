6c8114754ab3e124a2acd760afe19df280c36aff
Benjamin Mack <benni@typo3.org>
2015-06-20 18:26:25 +0200

    [FEATURE] Introduce "Application" classes for all entry points
    In an effort to move away from TYPO3_MODE and global code
    an instance that acts as an entry point has been introduced. The
    Application class which serves as a wrapper for setting up the
    Bootstrap and calling the request has been introduced.
    
    Additionally, new Applications (like typo3_console) can benefit
    from the flexible way of registering RequestHandlers on their own
    while still using the bootstrap or only parts of it.
    
    Resolves: #67808
    Releases: master
    Change-Id: I20c92d0bc8df5d50d76922a8dfe1cbd898e6f1dd
    Reviewed-on: http://review.typo3.org/40493
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ Classes/Core/ApplicationInterface.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3\CMS\Core\Core;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * The base ApplicationInterface which
+ * is used for all Entry Points for TYPO3, may it be
+ * Frontend, Backend, Install Tool or Command Line.
+ */
+interface ApplicationInterface {
+
+	/**
+	 * Starting point
+	 *
+	 * @param callable $execute
+	 * @return void
+	 */
+	public function run(callable $execute = NULL);
+}
