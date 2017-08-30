c592d5456a696c69622f0e85b80c3018bd9fe9a7
Christian Kuhn <lolli@schwarzbu.ch>
2017-08-30 12:39:07 +0200

    [TASK] Install tool: Use ext:core messaging
    The install tool brought its own "status message" class
    structure since the 6.2 refactoring. This is used at many
    places in the install tool for message handling.
    
    The core has a very similar class construct "Messaging"
    with only little dependencies, too. To simplify a later
    separation of 'install tool' and 'installer' the internal
    status message class structure is removed and transitioned
    to the core Messaging structure. to get rid of just
    another special thing the install tool does.
    
    The ext:core FlashMessage and FlashMessageQueue now both
    implement the \JsonSerialize interface. This allows direct
    json_encode() calls on these objects, helpful for instance
    for ajax responses.
    
    In ext:install "Environment checks" suhosin specific checks
    have been removed since the project is dead and only has a
    pre-alpha php 7.0 fork, so probably nobody is using
    that with the given core PHP constraints anymore.
    
    Change-Id: Ifecd3cd4889d8db5aaf3e87f317c98be706ae82b
    Resolves: #82257
    Releases: master
    Reviewed-on: https://review.typo3.org/53835
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

---
+++ Tests/Unit/Messaging/FlashMessageTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3\CMS\Core\Tests\Unit\Messaging;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * Test case
+ */
+class FlashMessageTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function jsonSerializeReturnsArrayWithDetailData()
+    {
+        $message = new FlashMessage('aMessage', 'aTitle', FlashMessage::INFO);
+        $expected = [
+            'severity' => FlashMessage::INFO,
+            'title' => 'aTitle',
+            'message' => 'aMessage',
+        ];
+        $this->assertEquals($expected, $message->jsonSerialize());
+    }
+}
