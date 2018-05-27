4aa8fef58495ffeb337bb82e878f6c75beb12ad5
Benni Mack <benni@typo3.org>
2018-05-27 17:11:46 +0200

    [BUGFIX] Clean up error handler for site handling
    This patch handles several issues related to the new
    site error handling introduced in TYPO3 v9.2.0:
    
    1. It adds unit tests
    2. It adds a missing exception when an error handler
    is configured which does not implement the PageErrorHandlerInterface
    3. It fixes one minor issue in PageContentErrorHandler
    where a wrong "InvalidArgumentException" was thrown
    4. All PageErrorHandler logic was moved from EXT:frontend
    to EXT:core, as this would be a penalty across packages -
    the "Site" entity depends on them, so they must go to the same
    package (core).
    
    Releases: master
    Resolves: #85101
    Change-Id: Ibdc05024abd7c719dd8d5dcb3388bf1679b69990
    Reviewed-on: https://review.typo3.org/57057
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

---
+++ Classes/Error/PageErrorHandler/PageErrorHandlerInterface.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace TYPO3\CMS\Core\Error\PageErrorHandler;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Page error handler interface, used to jump in for Frontend-related calls
+ *
+ * Needs to be implemented by all custom PHP-related Page Error Handlers.
+ */
+interface PageErrorHandlerInterface
+{
+    public function handlePageError(ServerRequestInterface $request, string $message, array $reasons = []): ResponseInterface;
+}
