589dcb7ae25b7896a7e01d09e161c3c3e8b15b11
Oliver Bartsch <bo@cedev.de>
2022-03-18 02:00:36 +0100

    [TASK] Move TypoLinkCodecService to EXT:core
    EXT:core unfortunately has a couple of dependencies
    to EXT:frontend. One of them are TypoLinkCodecService.
    
    Since this service does not have any context, only related
    to EXT:frontend, it is now moved to EXT:core, resolving this
    cross dependency.
    
    This also allows to use the service in the DataHandler
    instead of duplicating parts of the code, which was
    previously done to prevent any EXT:frontend dependency
    in the DataHandler.
    
    Resolves: #97217
    Releases: main
    Change-Id: Ib24bfb530fd0ac486ac30b09f43717c95c98c292
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73993
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

---
+++ Tests/Unit/LinkHandling/TypoLinkCodecServiceTest.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Unit\LinkHandling;
+
+use TYPO3\CMS\Core\LinkHandling\TypoLinkCodecService;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * Test case
+ */
+class TypoLinkCodecServiceTest extends UnitTestCase
+{
+    protected TypoLinkCodecService $subject;
+
+    /**
+     * Set up test subject
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->subject = new TypoLinkCodecService();
+    }
+
+    /**
+     * @test
+     * @dataProvider encodeReturnsExpectedResultDataProvider
+     * @param array $parts
+     * @param string $expected
+     */
+    public function encodeReturnsExpectedResult(array $parts, string $expected): void
+    {
+        self::assertSame($expected, $this->subject->encode($parts));
+    }
+
+    /**
+     * @return array
+     */
+    public function encodeReturnsExpectedResultDataProvider(): array
+    {
+        return [
+            'empty input' => [
+                [
+                    'url' => '',
+                    'target' => '',
+                    'class' => '',
+                    'title' => '',
+                    'additionalParams' => '',
+                ],
+                '',
+            ],
+            'full parameter usage' => [
+                [
+                    'url' => '19',
+                    'target' => '_blank',
+                    'class' => 'css-class',
+                    'title' => 'testtitle with whitespace',
+                    'additionalParams' => '&x=y',
+                ],
+                '19 _blank css-class "testtitle with whitespace" &x=y',
+            ],
+            'crazy title and partial items only' => [
+                [
+                    'url' => 'foo',
+                    'title' => 'a "link\\ ti\\"tle',
+                ],
+                'foo - - "a \\"link\\\\ ti\\\\\\"tle"',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider decodeReturnsExpectedResultDataProvider
+     * @param string $typoLink
+     * @param array $expected
+     */
+    public function decodeReturnsExpectedResult(string $typoLink, array $expected): void
+    {
+        self::assertSame($expected, $this->subject->decode($typoLink));
+    }
+
+    /**
+     * @return array
+     */
+    public function decodeReturnsExpectedResultDataProvider(): array
+    {
+        return [
+            'empty input' => [
+                '',
+                [
+                    'url' => '',
+                    'target' => '',
+                    'class' => '',
+                    'title' => '',
+                    'additionalParams' => '',
+                ],
+            ],
+            'simple id input' => [
+                '19',
+                [
+                    'url' => '19',
+                    'target' => '',
+                    'class' => '',
+                    'title' => '',
+                    'additionalParams' => '',
+                ],
+            ],
+            'external url with target' => [
+                'www.web.de _blank',
+                [
+                    'url' => 'www.web.de',
+                    'target' => '_blank',
+                    'class' => '',
+                    'title' => '',
+                    'additionalParams' => '',
+                ],
+            ],
+            'page with class' => [
+                '42 - css-class',
+                [
+                    'url' => '42',
+                    'target' => '',
+                    'class' => 'css-class',
+                    'title' => '',
+                    'additionalParams' => '',
+                ],
+            ],
+            'page with title' => [
+                '42 - - "a link title"',
+                [
+                    'url' => '42',
+                    'target' => '',
+                    'class' => '',
+                    'title' => 'a link title',
+                    'additionalParams' => '',
+                ],
+            ],
+            'page with crazy title' => [
+                '42 - - "a \\"link\\\\ ti\\\\\\"tle"',
+                [
+                    'url' => '42',
+                    'target' => '',
+                    'class' => '',
+                    'title' => 'a "link\\ ti\\"tle',
+                    'additionalParams' => '',
+                ],
+            ],
+            'page with title and parameters' => [
+                '42 - - "a link title" &x=y',
+                [
+                    'url' => '42',
+                    'target' => '',
+                    'class' => '',
+                    'title' => 'a link title',
+                    'additionalParams' => '&x=y',
+                ],
+            ],
+            'page with complex title' => [
+                '42 - - "a \\"link\\" title with \\\\" &x=y',
+                [
+                    'url' => '42',
+                    'target' => '',
+                    'class' => '',
+                    'title' => 'a "link" title with \\',
+                    'additionalParams' => '&x=y',
+                ],
+            ],
+            'full parameter usage' => [
+                '19 _blank css-class "testtitle with whitespace" &X=y',
+                [
+                    'url' => '19',
+                    'target' => '_blank',
+                    'class' => 'css-class',
+                    'title' => 'testtitle with whitespace',
+                    'additionalParams' => '&X=y',
+                ],
+            ],
+        ];
+    }
+}
