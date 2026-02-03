18b8f02995a63b699c6689d887b981f009f4f012
Matthias Vogel <typo3@kanti.de>
2026-02-03 12:38:39 +0100

    [FEATURE] Add Badge for Slide Mode in Layout Module
    Editors now have more information about what slideMode is used.
    There is also a short description of what it does.
    
    Resolves: #108842
    Releases: main
    Change-Id: Ifd86fe6912f58ea334c079f43e2633b56d5b9be6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92643
    Tested-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Tested-by: Simon Praetorius <simon@praetorius.me>
    Reviewed-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Reviewed-by: Simon Praetorius <simon@praetorius.me>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>

---
+++ Classes/Page/ContentSlideMode.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Page;
+
+enum ContentSlideMode
+{
+    case None;
+    case Slide;
+    case Collect;
+    case CollectReverse;
+
+    public static function tryFrom(?string $slideMode): ContentSlideMode
+    {
+        return match ($slideMode) {
+            'slide' => self::Slide,
+            'collect' => self::Collect,
+            'collectReverse' => self::CollectReverse,
+            default => self::None,
+        };
+    }
+
+    public function getValue(): string
+    {
+        return \lcfirst($this->name);
+    }
+}
